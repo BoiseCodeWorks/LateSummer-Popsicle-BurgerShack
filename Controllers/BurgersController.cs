b416f08de6503423289cfce99e6be04e0cadb79a
porterwilcox <wilcox.porter@gmail.com>
2019-08-22 16:35:40 -0600

    Ronnat

--- Controllers/BurgersController.cs
+++ Controllers/BurgersController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using BurgerShack.Data;
 using BurgerShack.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,18 +10,43 @@ namespace BurgerShack.Controllers
     [ApiController]
     public class BurgersController : ControllerBase
     {
+        private readonly BurgersRepository _repo;
+
+        public BurgersController(BurgersRepository repo)
+        {
+        _repo =repo;    
+        }
+
         [HttpGet]
         // TODO Come back to this
         public ActionResult<IEnumerable<Burger>> Get()
         {
-            return null;
+            try
+            {
+                return Ok(_repo.GetBurgers());
+            }
+            catch (Exception e)
+            {
+                
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpGet("{id}")]
         // STUB
         public ActionResult<Burger> Get(int id)
         {
-            return null;
+            try
+            {
+                return Ok(_repo.GetById(id))
+                
+            }
+            catch (Exception e)
+            {
+                
+                
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpPost]
